0f3807bf263167cb1b5cc8ba6f78cc52cb6ba18d
Beyond Ma <beyond.ma@activenetwork.com>
2020-12-29 17:04:21 +0800

    optimize the ssrunner trigger

--- ssrunner_trigger.sh
+++ ssrunner_trigger.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/ash
 
 sleep 5
 
 selenium-side-runner --server "http://chromedriver:$CHROMEDRIVER_PORT" --output-directory "/selenium_side_runner/$SELE_SIDE_RUNNER_REPORT_FOLDER" "/selenium_side_runner/$SELE_SIDE_RUNNER_TEST_FOLDER/*.side"
+
